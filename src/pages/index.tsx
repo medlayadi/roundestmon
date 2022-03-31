2e041a9e54a8c4bd74b927d3cd320405c8c8326c
unknown <layadi.mohamed9@gmail.com>
2022-03-31 17:59:20 +0000

    add random pokemon id fetcher

---
+++ src/pages/index.tsx
@@ -0,0 +1,20 @@
+import { getOptionsForVote } from "@/utils/getRandomPokemon";
+import { trpc } from "@/utils/trpc";
+
+const Home = () => {
+  const [first, second] = getOptionsForVote();
+
+  return (
+    <div className="h-screen w-screen flex flex-col justify-center items-center">
+      <div className="text-2xl text-center">Which Pokémon is rounder?</div>
+      <div className="p-2"></div>
+      <div className="border rounded p-8 flex justify-between max-w-2xl items-center">
+        <div className="w-16 h-16 bg-red-800">{first}</div>
+        <div className="p-8">Vs</div>
+        <div className="w-16 h-16 bg-red-800">{second}</div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 module.exports = {
   mode: "jit",
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
+  purge: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
